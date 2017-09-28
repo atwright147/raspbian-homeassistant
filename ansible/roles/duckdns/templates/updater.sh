83fb5274de8e83a112737a3d565e3528f0b9b5ba
Andy Wright <atwright147@gmail.com>
2017-09-28 17:56:12 +0100

    provide full path to curl

--- ansible/roles/duckdns/templates/updater.sh
+++ ansible/roles/duckdns/templates/updater.sh
@@ -1,1 +1,1 @@
-echo url="https://www.duckdns.org/update?domains={{ server.hostname }}&token={{ duckdns.token }}&ip=" | curl -k -o /var/log/duckdns.log -K -
+echo url="https://www.duckdns.org/update?domains={{ server.hostname }}&token={{ duckdns.token }}&ip=" | /usr/bin/curl -k -o /var/log/duckdns.log -K -
